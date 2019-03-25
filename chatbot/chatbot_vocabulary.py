5fb3882b0606ba024d1bdd75a975d99da9418a1a
onebigcabbage <zhongxiangcai17@gmail.com>
2019-03-25 19:24:32 +0800

    A funny chatbot

---
+++ chatbot/chatbot_vocabulary.py
@@ -0,0 +1,76 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import torch
+from torch.jit import script, trace
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import csv
+import random
+import re
+import os
+import unicodedata
+import codecs
+from io import open
+import itertools
+import math
+
+
+# Load and trim data
+
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+class Voc:
+    def __init__(self, name):
+        self.name = name
+        self.trimmed = False
+        # 词-索引
+        self.word2index = {}
+        # 词频
+        self.word2count = {}
+        # 索引-词
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        # 词量
+        self.num_words = 3
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+    # 去除词频小于给定值的词
+    def trim(self, min_count):
+        if self.trimmed:
+            return
+        self.trimmed = True
+
+        keep_words = []
+
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep_words {} / {} = {:.4f}'.format(
+            len(keep_words), len(self.word2index), len(keep_words) / len(self.word2index)
+        ))
+
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 3
+
+        for word in keep_words:
+            self.addWord(word)
